f9eeba4c8db7d12895254f159e8e457c1e4cd9a8
Nayan <niranzanroy1@gmail.com>
2026-06-30 12:28:02 +0600

    portfolio design completed

--- client/src/components/portfolio/Portfolio.jsx
+++ client/src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import product1 from "../../assets/product-1.jpg"
 
 const Portfolio = () => {
   return (
@@ -8,31 +9,107 @@ const Portfolio = () => {
               <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
         </div>
         <div className='w-full mx-auto px-3 z-3 sm:max-w-135 md:max-w-180 lg:max-w-240 xl:max-w-285 xxl:max-w-330'>
-              <div className='flex flex-col flex-wrap   md:flex-row '>
-                  <div className='bg-blue-700 mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
-                      <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" w-10 h-12 bg-accent">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-                          </svg>
+              
+              <ul className='mb-5 text-center flex items-center justify-center gap-2.5 sm:gap-5 flex-wrap font-roboto'>
+                  <li className='cursor-pointer inline-block text-[14px] sm:text-[18px] font-medium leading-none transition duration-300 hover:text-accent'>All</li>
+                  <li className='cursor-pointer inline-block text-[14px] sm:text-[18px] font-medium leading-none transition duration-300 hover:text-accent'>Figma_Design</li>
+                  <li className='cursor-pointer inline-block text-[14px]  sm:text[18px]  font-medium leading-none transition duration-300 hover:text-accent'>Frond-End_Design</li>
+                  <li className='cursor-pointer inline-block text-[14px]  sm:text[18px]  font-medium leading-none transition duration-300 hover:text-accent'>Back-End_API</li>
+                  <li className='cursor-pointer inline-block text-[14px]  sm:text[18px]  font-medium leading-none transition duration-300 hover:text-accent'>Full-Stack_Development</li>
+              </ul>
 
+
+              <div className='flex flex-col flex-wrap   md:flex-row '>
+                   <div className='mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
+                      <div className='relative truncate group'>
+                          <img className='w-full object-cover transition duration-300 group-hover:scale-[1.1]'  src={product1} alt="product1" />
+                          <div className='absolute inset-0  bg-layer flex flex-col transition duration-300  opacity-0 group-hover:opacity-100'>
+                              <h4 className='text-[14px] p-4 inline-block bg-accent font-playfair font-normal text-white'>Product Name</h4>
+                             
+                              <button className=' self-center justify-self-center mt-8.5 text-[16px] bg-accent py-3 px-5 rounded-[5px] text-white'>Show Details</button>
+                             
+                          </div>
                       </div>
 
                   </div>
-                  <div className=' bg-amber-400 mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
-                       section2
+                   
+
+                   <div className='mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
+                      <div className='relative truncate group'>
+                          <img className='w-full object-cover transition duration-300 group-hover:scale-[1.1]'  src={product1} alt="product1" />
+                          <div className='absolute inset-0  bg-layer flex flex-col transition duration-300  opacity-0 group-hover:opacity-100'>
+                              <h4 className='text-[14px] p-4 inline-block bg-accent font-playfair font-normal text-white'>Product Name</h4>
+                             
+                              <button className=' self-center justify-self-center mt-8.5 text-[16px] bg-accent py-3 px-5 rounded-[5px] text-white'>Show Details</button>
+                             
+                          </div>
+                      </div>
+
                   </div>
-                  <div className='bg-amber-950 mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
-                       section3
+
+
+                  <div className='mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
+                      <div className='relative truncate group'>
+                          <img className='w-full object-cover transition duration-300 group-hover:scale-[1.1]'  src={product1} alt="product1" />
+                          <div className='absolute inset-0  bg-layer flex flex-col transition duration-300  opacity-0 group-hover:opacity-100'>
+                              <h4 className='text-[14px] p-4 inline-block bg-accent font-playfair font-normal text-white'>Product Name</h4>
+                             
+                              <button className=' self-center justify-self-center mt-8.5 text-[16px] bg-accent py-3 px-5 rounded-[5px] text-white'>Show Details</button>
+                             
+                          </div>
+                      </div>
+
                   </div>
-                  <div className='bg-amber-950 mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
-                       section4
+
+                  <div className='mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
+                      <div className='relative truncate group'>
+                          <img className='w-full object-cover transition duration-300 group-hover:scale-[1.1]'  src={product1} alt="product1" />
+                          <div className='absolute inset-0  bg-layer flex flex-col transition duration-300  opacity-0 group-hover:opacity-100'>
+                              <h4 className='text-[14px] p-4 inline-block bg-accent font-playfair font-normal text-white'>Product Name</h4>
+                             
+                              <button className=' self-center justify-self-center mt-8.5 text-[16px] bg-accent py-3 px-5 rounded-[5px] text-white'>Show Details</button>
+                             
+                          </div>
+                      </div>
+
                   </div>
-                  <div className='bg-amber-950 mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
-                       section5
+
+
+                  <div className='mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
+                      <div className='relative truncate group'>
+                          <img className='w-full object-cover transition duration-300 group-hover:scale-[1.1]'  src={product1} alt="product1" />
+                          <div className='absolute inset-0  bg-layer flex flex-col transition duration-300  opacity-0 group-hover:opacity-100'>
+                              <h4 className='text-[14px] p-4 inline-block bg-accent font-playfair font-normal text-white'>Product Name</h4>
+                             
+                              <button className=' self-center justify-self-center mt-8.5 text-[16px] bg-accent py-3 px-5 rounded-[5px] text-white'>Show Details</button>
+                             
+                          </div>
+                      </div>
+
                   </div>
-                  <div className='bg-amber-950 mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
-                       section6
+
+                  <div className='mb-7.5 md:w-1/2 md:px-3 lg:w-1/3 '>
+                      <div className='relative truncate group'>
+                          <img className='w-full object-cover transition duration-300 group-hover:scale-[1.1]'  src={product1} alt="product1" />
+                          <div className='absolute inset-0  bg-layer flex flex-col transition duration-300  opacity-0 group-hover:opacity-100'>
+                              <h4 className='text-[14px] p-4 inline-block bg-accent font-playfair font-normal text-white'>Product Name</h4>
+                             
+                              <button className=' self-center justify-self-center mt-8.5 text-[16px] bg-accent py-3 px-5 rounded-[5px] text-white'>Show Details</button>
+                             
+                          </div>
+                      </div>
+
                   </div>
+
+
+
+                
+
+
+
+
+
+
               </div>
         </div>
     </div>
