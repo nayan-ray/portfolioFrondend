3e338ac0e1720de6909df428f124e78d046bdb21
Nayan <niranzanroy1@gmail.com>
2026-07-09 11:46:39 +0600

    icon updated

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className='py-7.5 bg-[#f3f1f0] text-[#898989] text-[14px] font-roboto'>
        <div className='text-center'>
- 
-   
+           <div>
+               <a
+                 href="https://github.com/nayan-ray"
+                 target="_blank"
+                 rel="noopener noreferrer"
+                 className="inline-flex items-center gap-2 text-gray-700 hover:text-black transition"
+                >
+                   <FaGithub size={24} />
+                   <span>GitHub</span>
+                 </a>
+           </div>
+        
            <div className='text-[16px] my-2 text-accent1'>
                Designed And Developed By <span>Nayan Kumar Ray</span>
            </div>
